3bf3c5c3f5140e278daf671e47d16a7ee59a951f
Administrator <Administrator@Lenovo-480766>
2018-03-28 17:55:37 +0800

    commit

--- src/main/java/com/zhelee/info/DemoApplication.java
+++ src/main/java/com/zhelee/info/DemoApplication.java
@@ -2,8 +2,10 @@ package com.zhelee.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.zhelee")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/zhelee/info/controllers/HelloController.java
+++ src/main/java/com/zhelee/info/controllers/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class HelloController {
 	
 	@Value("${message.welcome}")
